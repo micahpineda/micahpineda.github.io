a96a13a2f0b4179131914a17174b7794104444fc
micahpineda <micah6693@gmail.com>
2025-09-04 17:58:24 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,29 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
+  createPlatform(300, 650, 200, 10);
+  createPlatform(550, 550, 200, 10);
+  createPlatform(700, 450, 200, 10);
+  createPlatform(800, 350, 200, 10);
+  createPlatform(500, 250, 200, 10);
 
 
 
     // TODO 3 - Create Collectables
-
-
+    createCollectable("max", 300 , 300 , 0.9, 0.7);
+    createCollectable("max", 10 , 300 , 0.9, 0.7);
+    createCollectable("max", 600 , 200 , 0.9, 0.7);
+    createCollectable("max", 900 , 300 , 0.9, 0.7);
 
     
     // TODO 4 - Create Cannons
-
+    createCannon("left", 400, 1000);
+    createCannon("top", 550, 1000);
+    createCannon("right", 200, 1000);
 
     
     
